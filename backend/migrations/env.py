712438482119fcfee854f854780f0229d27717f0
Sumanth <srisatyasumanth2001@gmail.com>
2026-06-25 23:39:39 +0530

    Deleted migration versions and redone migration for supabase

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -3,15 +3,28 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from app.core.database import Base
-from app.models import user, conversation, message
+from app.models import (
+    user,
+    conversation,
+    message,
+    file,
+    document,
+    repository,
+    embedding,
+    saved_response,
+)
 target_metadata = Base.metadata
 
-
+import os
 from alembic import context
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+database_url = os.getenv("DATABASE_URL")
+
+if database_url:
+    config.set_main_option("sqlalchemy.url", database_url.replace("%", "%%"))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
